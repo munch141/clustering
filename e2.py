3102d9104bea086f5c01b76390433255b6acfea2
Ricardo Münch <munch_141@hotmail.com>
2017-03-17 01:27:16 -0400

    e2 listo

--- e2.py
+++ e2.py
@@ -14,12 +14,36 @@ from load_data import *
 
 
 data = load_iris("iris.data")
+ejemplos = zip(*data)[0]
+setosa = [tuple(e) for e in ejemplos[:50]]
+versicolor = ejemplos[50:100]
+virginica = ejemplos[100:]
 
 ks = [2,3,4,5]
 for k in range(2, 6):
-    clusters, centroids = k_means(k, zip(*data)[0])
+    clusters, centroids = k_means(k, ejemplos)
     graficar_clusters_iris(clusters,
                            centroids,
                            "plots/iris_clusters_{0}.png".format(k),
                            False)
 graficar_iris("plots/iris_plot.png", False)
+
+c2, _ = k_means(2, ejemplos)
+c3, _ = k_means(3, ejemplos)
+
+if len(c2.values()[0]) == 53:
+    c = [tuple(x) for x in c2.values()[0]]
+else:
+    c = [tuple(x) for x in c2.values()[1]]
+
+print "Con k = 2 hubo {0} clasificaciones erroneas (ver grafica).".format(len(set(c) - set(setosa)))
+
+if len(c3.values()[0]) == 50:
+    c1, c2 = 1, 2
+elif len(c3.values()[1]) == 50:
+    c1, c2 = 0, 2
+else:
+    c1, c2 = 0, 1
+
+print "Con k = 3 hubo {0} clasificaciones erroneas."\
+      .format(abs(len(c3.values()[c1])-len(c3.values()[c2])))
---
+++ e3.py
@@ -0,0 +1,11 @@
+"""
+Proyecto 2 de Inteligencia Artificial 2.
+Enero-Marzo 2017.
+Hecho por:
+    Ricardo Munch.       Carnet: 11-10684.
+    Valentina Hernandez. Carnet: 10-10352.
+
+Este archivo contiene el codigo para generar los resultados del ejercicio 2.
+"""
+
+
